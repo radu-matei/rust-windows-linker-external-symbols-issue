c2f7df931a8675ad5386e14ea4c4339a5819d28b
Radu M <root@radu.sh>
2021-06-07 18:17:27 -0700

    Simplify repro some more
    Signed-off-by: Radu M <root@radu.sh>

--- crate/crate-with-external-symbols/src/external_symbols.rs
+++
@@ -1,3 +0,0 @@
-extern "C" {
-    pub fn foo(x: i32, y: i32) -> i32;
-}
--- crate/crate-with-external-symbols/src/lib.rs
+++ crate/crate-with-external-symbols/src/lib.rs
@@ -1,7 +1,9 @@
-mod external_symbols;
-
 pub fn test_not_using_extern() {}
 
 pub fn abc() {
-    let _ = unsafe { external_symbols::foo(1, 2) };
+    let _ = unsafe { foo(1, 2) };
+}
+
+extern "C" {
+    fn foo(x: i32, y: i32) -> i32;
 }
